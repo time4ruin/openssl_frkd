1dedf951d20f477a9c3d07459e11b32ae561611d
time4ruin <time4_ruin@naver.com>
2022-02-13 03:43:46 -0800

    change run.sh

--- run.sh
+++ run.sh
@@ -7,16 +7,14 @@ echo "abc123" | sudo -S ln -Tfs /usr/bin/x86_64-linux-gnu-as /usr/bin/as
 echo $(ls -al /usr/bin/as)
 make clean > /dev/null 2>&1
 make > /dev/null 2>&1
-echo $(ls -al ./tests/test_suite_mpi)
-echo $(ls -al ./library/bignum.o)
-cp ./tests/test_suite_mpi.datax ./
-echo "abc123" | sudo -S perf stat -e page-faults,br_inst_retired.conditional,br_misp_retired.conditional ./tests/test_suite_mpi >/dev/null
+echo $(ls -al ./test/bntest)
+echo $(ls -al ./crypto/bn/libcrypto-shlib-bn_exp.o)
+echo "abc123" | sudo -S perf stat -e page-faults,br_inst_retired.conditional,br_misp_retired.conditional ./test/bntest >/dev/null
 
 echo "abc123" | sudo -S ln -Tfs $C /usr/bin/as
 echo $(ls -al /usr/bin/as)
 make clean > /dev/null 2>&1
 make > /dev/null 2>&1
-echo $(ls -al ./tests/test_suite_mpi)
-echo $(ls -al ./library/bignum.o)
-cp ./tests/test_suite_mpi.datax ./
-echo "abc123" | sudo -S perf stat -e page-faults,br_inst_retired.conditional,br_misp_retired.conditional ./tests/test_suite_mpi >/dev/null
+echo $(ls -al ./test/bntest)
+echo $(ls -al ./crypto/bn/libcrypto-shlib-bn_exp.o)
+echo "abc123" | sudo -S perf stat -e page-faults,br_inst_retired.conditional,br_misp_retired.conditional ./test/bntest >/dev/null

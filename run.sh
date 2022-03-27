bc8868c1ab8dcee07f6995907a57a75f3dba7956
time4ruin <time4_ruin@naver.com>
2022-03-26 22:19:32 -0700

    runsh

--- run.sh
+++ run.sh
@@ -4,7 +4,9 @@ argc=$#
 if [ 1 -eq $argc ]
 then
     args_1=$1
-    echo "directory of custom gcc/as : "$args_1
+    args_2=$2
+    echo "sudo passwd : "$args_1
+    echo "directory of custom gcc/as : "$args_2
 
     A=$(which as)
     B=$(ls -l $A)
@@ -14,26 +16,27 @@ then
     E=$(ls -l $A)
     F=${B#*-\> }
 
-    echo "abc123" | sudo -S ln -Tfs $args_1/as.out /usr/bin/as
-    echo "abc123" | sudo -S ln -Tfs $args_1/gcc.out /usr/bin/gcc
+    echo $args_1 | sudo -S ln -Tfs $args_2/as.out /usr/bin/as
+    echo $args_1 | sudo -S ln -Tfs $args_2/gcc.out /usr/bin/gcc
     echo $(ls -al /usr/bin/gcc)
     echo $(ls -al /usr/bin/as)
-    ./Configure --prefix=/home/jh/Desktop/openssl_frkd --openssldir=/home/jh/Desktop/openssl_frkd -Wl,-rpath,/home/jh/Desktop/openssl_frkd
+    ./Configure
     make
-    echo "abc123" | sudo -S perf stat -o result_aligned.txt -e page-faults,br_inst_retired.conditional,br_misp_retired.conditional ./test/bntest >/dev/null
-    echo "abc123" | sudo -S echo $(ls -al ./test/bntest) >> result_aligned.txt
-    echo "abc123" | sudo -S echo $(ls -al ./crypto/bn/libcrypto-shlib-bn_exp.o) >> result_aligned.txt
+    echo $args_1| sudo -S perf stat -o result_perf_aligned.txt -e page-faults,br_inst_retired.conditional,br_misp_retired.conditional ./test/bntest >/dev/null
+    echo $(cat result_perf_aligned.txt) > result_aligned.txt
+    echo $(ls -al ./test/bntest) >> result_aligned.txt
+    echo $(ls -al ./crypto/bn/libcrypto-shlib-bn_exp.o) >> result_aligned.txt
 
-
-    echo "abc123" | sudo -S ln -Tfs $C /usr/bin/as
-    echo "abc123" | sudo -S ln -Tfs $F /usr/bin/gcc
+    echo $args_1 | sudo -S ln -Tfs $C /usr/bin/as
+    echo $args_1 | sudo -S ln -Tfs $F /usr/bin/gcc
     echo $(ls -al /usr/bin/gcc)
     echo $(ls -al /usr/bin/as)
-    ./Configure --prefix=/home/jh/Desktop/openssl_frkd --openssldir=/home/jh/Desktop/openssl_frkd -Wl,-rpath,/home/jh/Desktop/openssl_frkd
+    ./Configure
     make
-    echo "abc123" | sudo -S perf stat -o result_raw.txt -e page-faults,br_inst_retired.conditional,br_misp_retired.conditional ./test/bntest >/dev/null
-    echo "abc123" | sudo -S echo $(ls -al ./test/bntest) >> result_raw.txt
-    echo "abc123" | sudo -S echo $(ls -al ./crypto/bn/libcrypto-shlib-bn_exp.o) >> result_raw.txt
+    echo $args_1 | sudo -S perf stat -o result_perf_raw.txt -e page-faults,br_inst_retired.conditional,br_misp_retired.conditional ./test/bntest >/dev/null
+    echo $(cat result_perf_raw.txt) > result_raw.txt
+    echo $(ls -al ./test/bntest) >> result_raw.txt
+    echo $(ls -al ./crypto/bn/libcrypto-shlib-bn_exp.o) >> result_raw.txt
 else
     echo "usage: ./run.sh {directory of custom gcc/as}"
 fi

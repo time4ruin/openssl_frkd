43a361f3a537c40455d3ab85ad5da69d2a57c324
time4ruin <time4_ruin@naver.com>
2022-03-27 14:05:44 +0900

    runsh

--- run.sh
+++ run.sh
@@ -18,21 +18,22 @@ then
     echo "abc123" | sudo -S ln -Tfs $args_1/gcc.out /usr/bin/gcc
     echo $(ls -al /usr/bin/gcc)
     echo $(ls -al /usr/bin/as)
-    ./Configure > /dev/null 2>&1
-    make > /dev/null 2>&1
-    echo $(ls -al ./test/bntest)
-    echo $(ls -al ./crypto/bn/libcrypto-shlib-bn_exp.o)
-    echo "abc123" | sudo -S perf stat -e page-faults,br_inst_retired.conditional,br_misp_retired.conditional ./test/bntest >/dev/null
+    ./Configure --prefix=/home/jh/Desktop/openssl_frkd --openssldir=/home/jh/Desktop/openssl_frkd -Wl,-rpath,/home/jh/Desktop/openssl_frkd
+    make
+    echo "abc123" | sudo -S perf stat -o result_aligned.txt -e page-faults,br_inst_retired.conditional,br_misp_retired.conditional ./test/bntest >/dev/null
+    echo "abc123" | sudo -S echo $(ls -al ./test/bntest) >> result_aligned.txt
+    echo "abc123" | sudo -S echo $(ls -al ./crypto/bn/libcrypto-shlib-bn_exp.o) >> result_aligned.txt
+
 
     echo "abc123" | sudo -S ln -Tfs $C /usr/bin/as
     echo "abc123" | sudo -S ln -Tfs $F /usr/bin/gcc
     echo $(ls -al /usr/bin/gcc)
     echo $(ls -al /usr/bin/as)
-    ./Configure > /dev/null 2>&1
-    make > /dev/null 2>&1
-    echo $(ls -al ./test/bntest)
-    echo $(ls -al ./crypto/bn/libcrypto-shlib-bn_exp.o)
-    echo "abc123" | sudo -S perf stat -e page-faults,br_inst_retired.conditional,br_misp_retired.conditional ./test/bntest >/dev/null
+    ./Configure --prefix=/home/jh/Desktop/openssl_frkd --openssldir=/home/jh/Desktop/openssl_frkd -Wl,-rpath,/home/jh/Desktop/openssl_frkd
+    make
+    echo "abc123" | sudo -S perf stat -o result_raw.txt -e page-faults,br_inst_retired.conditional,br_misp_retired.conditional ./test/bntest >/dev/null
+    echo "abc123" | sudo -S echo $(ls -al ./test/bntest) >> result_raw.txt
+    echo "abc123" | sudo -S echo $(ls -al ./crypto/bn/libcrypto-shlib-bn_exp.o) >> result_raw.txt
 else
     echo "usage: ./run.sh {directory of custom gcc/as}"
 fi
